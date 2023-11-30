6809f01e0afb5b2b61d6750d860aeb867fcde66b
saifularafat <saifularafat.info@gmail.com>
2023-11-30 23:36:48 +0600

    mongodb database connection DONE!

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const app = express();
-const PORT = 5000
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const PORT = process.env.PORT || 5000;
+
+/* middleware */
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DATA_USER}:${process.env.DATA_PASS}@cluster0.guqonkt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Flowers Server Site is Start")
 })
 app.listen(PORT, () => {
-    console.log(`Flower site is running now ${PORT}`);
+    console.log(`Flower site is running of PORT a ${PORT}`);
 })
