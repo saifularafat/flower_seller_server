655931c8d0997ffe541562f35718b1a32ee8a53f
saifularafat <saifularafat.info@gmail.com>
2023-12-22 00:05:47 +0600

    flower api post the database

--- index.js
+++ index.js
@@ -26,27 +26,36 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // await client.connect();
-        const userCollection = client.db("flowersShop").collection("users");
-
+        const usersCollection = client.db("flowersShop").collection("users");
+        const flowersCollection = client.db("flowersShop").collection("allFlowers");
 
         /* user crate */
         app.get("/users", async (req, res) => {
-            const result = await userCollection.find().toArray();
+            const result = await usersCollection.find().toArray();
             res.send(result);
         });
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
-            const existingUser = await userCollection.findOne(query);
+            const existingUser = await usersCollection.findOne(query);
             if (existingUser) {
                 return res.send({ message: "You is already access!" });
             }
-            const result = await userCollection.insertOne(user);
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+        /* Flowers section  */
+        app.get("/allFlowers", async (req, res) => {
+            const result = await flowersCollection.find().toArray();
+            res.send(result);
+        })
+        app.post("/allFlowers", async (req, res) => {
+            const flower = req.body;
+            const result = await flowersCollection.insertOne(flower);
+            res.send(result)
+        })
 
-
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Slower Shop DataBase is successfully connected to MongoDB!");
     } finally {
         // await client.close();
