bc17765131e02e6a72a4ea6cab061f3484bcd938
saifularafat <saifularafat.info@gmail.com>
2024-01-18 23:22:54 +0600

    admin api path create the users arrary

--- index.js
+++ index.js
@@ -88,7 +88,17 @@ async function run() {
             const query = { email: email };
             const result = await usersCollection.find(query).toArray();
             res.send(result);
-        })
+        });
+        app.get("/users/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded?.email !== email) {
+                return res.send({ admin: false })
+            }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === "admin" };
+            res.send(result)
+        });
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
